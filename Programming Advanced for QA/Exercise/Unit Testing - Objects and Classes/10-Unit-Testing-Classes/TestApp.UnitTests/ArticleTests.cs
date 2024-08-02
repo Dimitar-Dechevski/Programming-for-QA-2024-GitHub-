4a26f859df3cd9919c8aa1f6618eec42276e8c93
Dimitar Dechevski <86205826+Dimitar-Dechevski@users.noreply.github.com>
2024-08-02 22:09:51 +0300

    commit

---
+++ Programming Advanced for QA/Exercise/Unit Testing - Objects and Classes/10-Unit-Testing-Classes/TestApp.UnitTests/ArticleTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ArticleTests
+{
+    private Article article;
+
+    [SetUp]
+    public void SetUp()
+    {
+        article = new Article();
+    }
+
+    [Test]
+    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    {
+        // Arrange
+        string[] input = new string[] { "VW Golf Bild", "Champions Wimbledon Times", "Invincibles Arsenal Guardian" };
+        Article expected = new Article();
+        for (int i = 0; i < input.Length; i++)
+        {
+            string[] tokens = input[0].Split(" ");
+            article.Title = tokens[0];
+            article.Content = tokens[1];
+            article.Author = tokens[2];
+            expected.ArticleList.Add(article);
+        }
+
+        // Act
+        Article actual = article.AddArticles(input);
+
+        // Assert
+        Assert.AreEqual(expected.ArticleList.Count, actual.ArticleList.Count);
+        Assert.AreEqual(expected.Title, actual.Title);
+        Assert.AreEqual(expected.Content, actual.Content);
+        Assert.AreEqual(expected.Author, actual.Author);
+    }
+
+    [Test]
+    public void Test_GetArticleList_SortsArticlesByTitle()
+    {
+        // Arrange
+        string[] input = new string[] { "VW Golf Bild", "Champions Wimbledon Times", "Invincibles Arsenal Guardian" };
+        string criteria = "title";
+        Article currentArticle = article.AddArticles(input);
+        string expected = $"Champions - Wimbledon: Times{Environment.NewLine}" +
+                          $"Invincibles - Arsenal: Guardian{Environment.NewLine}" +
+                          $"VW - Golf: Bild";
+
+        // Act
+        string actual = article.GetArticleList(currentArticle, criteria);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+    {
+        // Arrange
+        string[] input = new string[] { "VW Golf Bild", "Champions Wimbledon Times", "Invincibles Arsenal Guardian" };
+        string criteria = "";
+        Article currentArticle = article.AddArticles(input);
+        string expected = "";
+
+        // Act
+        string actual = article.GetArticleList(currentArticle, criteria);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ Programming Advanced for QA/Exercise/Unit Testing - Objects and Classes/10-Unit-Testing-Classes/TestApp.UnitTests/VehicleTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+using TestApp.Vehicle;
+
+namespace TestApp.UnitTests;
+
+public class VehicleTests
+{
+    private Vehicles vehicles;
+
+    [SetUp]
+    public void SetUp()
+    {
+        vehicles = new Vehicles();
+    }
+
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsSortedCatalogue()
+    {
+        // Arrange
+        string[] vehicles = new string[] { "Car/Skoda/Fabia/150", "Truck/Scania/S730/9910", "Car/Audi/Q8/376" };
+        string expected = $"Cars:{Environment.NewLine}Audi: Q8 - 376hp{Environment.NewLine}" +
+                          $"Skoda: Fabia - 150hp{Environment.NewLine}" +
+                          $"Trucks:{Environment.NewLine}Scania: S730 - 9910kg";
+
+        // Act
+        string actual = this.vehicles.AddAndGetCatalogue(vehicles);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsEmptyCatalogue_WhenNoDataGiven()
+    {
+        // Arrange
+        string[] vehicles = Array.Empty<string>();
+        string expected = $"Cars:{Environment.NewLine}Trucks:";
+
+        // Act
+        string actual = this.vehicles.AddAndGetCatalogue(vehicles);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
